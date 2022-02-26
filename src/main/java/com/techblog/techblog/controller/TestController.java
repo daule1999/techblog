4f9d1219e987c6d41e9c808083b1eb3a162b20f9
Dauleshwar kumar <37247963+daule1999@users.noreply.github.com>
2022-02-26 10:51:13 +0530

    Update TestController.java

--- src/main/java/com/techblog/techblog/controller/TestController.java
+++ src/main/java/com/techblog/techblog/controller/TestController.java
@@ -1,5 +1,15 @@
 package com.techblog.techblog.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "hello world";
+	}
     
 }
