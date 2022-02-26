5f2d3402dfd014950bccc8f3b69e6050758945cc
Dauleshwar kumar <37247963+daule1999@users.noreply.github.com>
2022-02-26 10:19:19 +0530

    Update TechblogApplication.java

--- src/main/java/com/techblog/techblog/TechblogApplication.java
+++ src/main/java/com/techblog/techblog/TechblogApplication.java
@@ -2,6 +2,7 @@ package com.techblog.techblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.utils.*;
 
 @SpringBootApplication
 public class TechblogApplication {
@@ -9,7 +10,11 @@ public class TechblogApplication {
 		super();
 	}
 	public static void main(String[] args) {
-		SpringApplication.run(TechblogApplication.class, args);
+		// SpringApplication.run(TechblogApplication.class, args);
+		SpringApplication app = new SpringApplication(TechblogApplication.class);
+        	app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
+		System.out.println("Server running at port  "+8083)
+        	app.run(args);
 	}
 
 }
