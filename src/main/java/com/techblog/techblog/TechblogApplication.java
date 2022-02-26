d5431fd7c131fe2a34e83879e019b0be36cb9701
Dauleshwar kumar <dauleshwark@gmail.com>
2022-02-26 04:19:37 +0000

    first commit

---
+++ src/main/java/com/techblog/techblog/TechblogApplication.java
@@ -0,0 +1,15 @@
+package com.techblog.techblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechblogApplication {
+	public TechblogApplication(){
+		super();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(TechblogApplication.class, args);
+	}
+
+}
