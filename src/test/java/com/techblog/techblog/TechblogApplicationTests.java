a082dc28d6e94ce9d8fa85136202bcd8fae30041
Dauleshwar kumar <37247963+daule1999@users.noreply.github.com>
2022-02-26 10:10:55 +0530

    Update TechblogApplicationTests.java

--- src/test/java/com/techblog/techblog/TechblogApplicationTests.java
+++ src/test/java/com/techblog/techblog/TechblogApplicationTests.java
@@ -3,7 +3,11 @@ package com.techblog.techblog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TechblogApplicationTests.class,
+  webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("fixedport")
 class TechblogApplicationTests {
 
 	@Test
